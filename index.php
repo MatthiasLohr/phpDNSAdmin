b099693c1be4b75632ad39d1dc02f8ce77712258
Matthias Lohr <mail@matthias-lohr.net>
2010-07-08 20:44:56 +0200

    first file structure

---
+++ index.php
@@ -0,0 +1,33 @@
+<?php
+
+/*
+ * This file is part of phpDNSAdmin.
+ * (c) 2010 Matthias Lohr - http://phpdnsadmin.sourceforge.net/
+ *
+ * jNav is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * jNav is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with phpDNSAdmin. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+?>
+<html>
+	<head>
+		<link rel="stylesheet" href="css/jquery/jquery.css" type="text/css" />
+		<script type="text/javascript" src="js/jquery-1.4.2.min.js" />
+		<script type="text/javascript" src="js/jquery-ui-1.8.2.min.js" />
+		<script type="text/javascript" src="js/jquery.jstree.js" />
+		<script type="text/javascript" src="js/jquery.tablesorter.min.js" />
+	<body>
+
+		
+	</body>
+</html>
