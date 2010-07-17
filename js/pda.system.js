85a0c6eaa67bef79cbd790cf9ee727edaf064ce3
Andi <andreas.litt@googlemail.com>
2010-07-17 16:26:48 +0200

    Removed debuging Code.

--- js/pda.system.js
+++ js/pda.system.js
@@ -15,43 +15,6 @@
  * You should have received a copy of the GNU General Public License
  * along with phpDNSAdmin. If not, see <http://www.gnu.org/licenses/>.
  */
-function var_dump(element, limit, depth)
-{
-	depth =	depth?depth:0;
-	limit = limit?limit:1;
-
-	returnString = '<ol>';
-
-	for(property in element)
-	{
-		//Property domConfig isn't accessable
-		if (property != 'domConfig')
-		{
-			returnString += '<li><strong>'+ property + '</strong> <small>(' + (typeof element[property]) +')</small>';
-
-			if (typeof element[property] == 'number' || typeof element[property] == 'boolean')
-				returnString += ' : <em>' + element[property] + '</em>';
-			if (typeof element[property] == 'string' && element[property])
-				returnString += ': <div style="background:#C9C9C9;border:1px solid black; overflow:auto;"><code>' +
-									element[property].replace(/</g, '&amp;lt;').replace(/>/g, '&amp;gt;') + '</code></div>';
-
-			if ((typeof element[property] == 'object') && (depth < limit))
-				returnString += var_dump(element[property], limit, (depth + 1));
-
-			returnString += '</li>';
-		}
-	}
-	returnString += '</ol>';
-
-	if(depth == 0)
-	{
-		winpop = window.open("", "","width=800,height=600,scrollbars,resizable");
-		winpop.document.write('<pre>'+returnString+ '</pre>');
-		winpop.document.close();
-	}
-
-	return returnString;
-}
 
 function login(username,password) {
   $.ajax({
