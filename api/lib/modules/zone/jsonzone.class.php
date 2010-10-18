78e0105abe7a8623a80b1022e694de75e65e83cd
Matthias Lohr <lohr@uni-trier.de>
2010-10-18 11:03:56 +0200

    template for json module

---
+++ api/lib/modules/zone/jsonzone.class.php
@@ -0,0 +1,89 @@
+<?php
+
+/*
+ * This file is part of phpDNSAdmin.
+ * (c) 2010 Matthias Lohr - http://phpdnsadmin.sourceforge.net/
+ *
+ * phpDNSAdmin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * phpDNSAdmin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with phpDNSAdmin. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+/**
+ * @package phpDNSAdmin
+ * @subpackage Zone
+ * @author Matthias Lohr <mail@matthias-lohr.net>
+ */
+
+/**
+ * @package phpDNSAdmin
+ * @subpackage Zone
+ */
+class JsonZone extends ZoneModule {
+	protected function __construct($config) {
+
+	}
+
+	public function getFeatures() {
+		return array(
+			'dnssec',
+			'rrtypes' => array(
+				'A', 'AAAA', 'AFSDB', 'CERT', 'CNAME', 'DNSKEY', 'DS', 'HINFO', 'KEY', 'LOC',
+				'MX', 'NAPTR', 'NS', 'NSEC', 'NSEC3', 'PTR', 'RP', 'RRSIG', 'SOA', 'SPF', 'SSHFP',
+				'SRV', 'TXT'
+			)
+		);
+	}
+
+	public static function getInstance($config) {
+
+	}
+
+	public function getRecordById(Zone $zone, $recordid) {
+
+	}
+
+	public function listRecordsByFilter(Zone $zone, array $filter = array()) {
+
+	}
+
+	public function listZones() {
+
+	}
+
+	public function recordAdd(Zone $zone, ResourceRecord $record) {
+
+	}
+
+	public function recordDelete(Zone $zone, $recordid) {
+
+	}
+
+	public function recordUpdate(Zone $zone, $recordid, ResourceRecord $record) {
+
+	}
+
+	public function zoneCreate(Zone $zone) {
+
+	}
+
+	public function zoneDelete(Zone $zone) {
+
+	}
+
+	public function zoneExists(Zone $zone) {
+
+	}
+
+}
+
+?>
