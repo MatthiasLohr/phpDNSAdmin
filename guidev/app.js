ad29db5f5b21071e2e57ecbe3d8d104d73c0f195
Andreas Litt <andreas.litt@googlemail.com>
2012-08-13 22:42:48 +0200

    Added Logo and improved deploy output.

--- guidev/app.js
+++ guidev/app.js
@@ -2,6 +2,5 @@ Ext.application({
     name: 'DNSAdmin',
     autoCreateViewport: true,
     launch: function() {
-        console.log('init', 'loaded!');
     }
 });
--- guidev/app/view/Viewport.js
+++ guidev/app/view/Viewport.js
@@ -1,36 +1,37 @@
 Ext.define('DNSAdmin.view.Viewport', {
-    extend: 'Ext.container.Viewport',
-    requires: [
-    ],
-    layout: 'fit',
-    initComponent: function() {
-        this.items = {
-            xtype: 'panel',
-            dockedItems: [{
-                dock: 'top',
-                xtype: 'toolbar',
-                height: 25,
-                items: [
-                    '->',
-                    '-', {
-                    xtype: 'component',
-                    html: 'phpDNSAdmin'
-                }]
-            }],
-            layout: {
-                type: 'hbox',
-                align: 'middle',
-                pack: 'center'
-            },
-            items: [{
-                xtype: 'button',
-                text: 'Click me',
-                handler: function() {
-                    alert('You clicked the button!');
-                }
-            }]
-        };
+			extend : 'Ext.container.Viewport',
+			requires : [],
+			layout : 'fit',
+			initComponent : function() {
+				this.items = {
+					xtype : 'panel',
+					dockedItems : [{
+								dock : 'top',
+								xtype : 'toolbar',
+								height : 35,
+								layout : {
+									type : 'hbox',
+									align : 'middle'
+								},
+								items : ['->', {
+											xtype : 'image',
+											src : 'resources/images/logo.png'
+										}]
+							}],
+					layout : {
+						type : 'hbox',
+						align : 'middle',
+						pack : 'center'
+					},
+					items : [{
+								xtype : 'button',
+								text : 'Click me',
+								handler : function() {
+									alert('You clicked the button!');
+								}
+							}]
+				};
 
-        this.callParent();
-    }
-});
+				this.callParent();
+			}
+		});
