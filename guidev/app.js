d3b2abe6a1f273eb83b919117ba6c3e231ab2172
Andreas Litt <andreas.litt@googlemail.com>
2012-10-08 16:55:21 +0200

    Added error handling (errorEvent)

--- guidev/app.js
+++ guidev/app.js
@@ -1,6 +1,6 @@
 Ext.application({
 			name : 'DNSAdmin',
-			controllers : ['LoginController'],
+			controllers : ['ErrorController', 'LoginController'],
 			models : ['Server'],
 
 			autoCreateViewport : true,
--- guidev/app/controller/LoginController.js
+++ guidev/app/controller/LoginController.js
@@ -4,107 +4,106 @@
  * logged in at the same time. (In one Browser)
  */
 Ext.define('DNSAdmin.controller.LoginController', {
-			extend : 'Ext.app.Controller',
-			views : ['LoginDialog', 'LoggedInText', 'Viewport'],
-			requires : ['DNSAdmin.Config'],
-			refs : [{
-						ref : 'loggedInText',
-						selector : 'loggedin'
-					}],
-			init : function() {
-				this.control({
-							'loginview button[action=login]' : {
-								click : this.loginBtnClicked
-							},
-							'loginview textfield' : {
-								specialkey : function(field, e) {
-									if (e.getKey() == e.ENTER) {
-										this.loginBtnClicked(field);
-									}
-								}
+	extend : 'Ext.app.Controller',
+	views : ['LoginDialog', 'LoggedInText', 'Viewport'],
+	requires : ['DNSAdmin.Config'],
+	refs : [{
+				ref : 'loggedInText',
+				selector : 'loggedin'
+			}],
+	init : function() {
+		this.control({
+					'loginview button[action=login]' : {
+						click : this.loginBtnClicked
+					},
+					'loginview textfield' : {
+						specialkey : function(field, e) {
+							if (e.getKey() == e.ENTER) {
+								this.loginBtnClicked(field);
 							}
-						});
+						}
+					}
+				});
 
-				this.application.on({
-							loggedInEvent : this.onLoggedInEvent,
-							notLoggedInEvent : this.onNotLoggedInEvent,
-							scope : this
-						});
+		this.application.on({
+					loggedInEvent : this.onLoggedInEvent,
+					notLoggedInEvent : this.onNotLoggedInEvent,
+					scope : this
+				});
 
-				this.checkIfLoggedIn();
-			},
+		this.checkIfLoggedIn();
+	},
 
-			checkIfLoggedIn : function() {
-				var app = this.application;
-				Ext.Ajax.request({
-							url : DNSAdmin.Config.apiBaseUrl + '/status',
+	checkIfLoggedIn : function() {
+		var app = this.application;
+		Ext.Ajax.request({
+			url : DNSAdmin.Config.apiBaseUrl + '/status',
 
-							success : function(response) {
-								var text = response.responseText;
-								var jsonResponse = Ext.JSON.decode(text);
-								if (jsonResponse.success) {
-									var loggedInView = Ext.widget('loggedin');
-									if (jsonResponse.loggedIn) {
-										// User is logged in, do something!
-										app.fireEvent('loggedInEvent',
-												jsonResponse.username);
-									} else {
-										// User is not logged in! Call
-										// notLoggedInEvent - Event
-										app.fireEvent('notLoggedInEvent');
-									}
-								} else {
-									// There was an Error on Server... need some
-									// Error handling here!
-									// TODO: Error Handling.
-								}
-							}
-						});
-			},
-
-			loginBtnClicked : function(srcElement) {
-				var win = srcElement.up('window'), form = win.down('form')
-						.getForm();
-				win.setLoading();
-				var app = this.application;
-				if (form.isValid()) {
-					Ext.getCmp('loginStatusLabel').hide();
-					form.submit({
-								success : function(form, action) {
-									if (action.result.loggedIn) {
-										app.fireEvent('loggedInEvent',
-												action.result.username);
-										// Login was successful
-										win.hide();
-									} else {
-										// Login failed
-										app.fireEvent('notLoggedInEvent');
-										Ext.getCmp('loginStatusLabel').show();
-									}
-									win.setLoading(false);
-								},
-								failure : function(form, action) {
-									win.setLoading(false);
-									Ext.Msg
-											.alert('Failed',
-													action.result.error);
-								}
-							});
+			success : function(response) {
+				var text = response.responseText;
+				var jsonResponse = Ext.JSON.decode(text);
+				if (jsonResponse.success) {
+					var loggedInView = Ext.widget('loggedin');
+					if (jsonResponse.loggedIn) {
+						// User is logged in, do something!
+						app.fireEvent('loggedInEvent', jsonResponse.username);
+					} else {
+						// User is not logged in! Call
+						// notLoggedInEvent - Event
+						app.fireEvent('notLoggedInEvent');
+					}
+				} else {
+					app.fireEvent('errorEvent', jsonResponse.error);
 				}
 			},
-
-			/*
-			 * Functions which will be called on Events.
-			 */
-			onLoggedInEvent : function(username, env) {
-				console.log('onLoggedInEvent');
-				var loggedInTextView = this.getLoggedInText();
-				loggedInTextView.setUsername(username);
-			},
-
-			onNotLoggedInEvent : function() {
-				var loginView = Ext.widget('loginview');
-				console.log(loginView);
-				loginView.show();
+			failure : function(response, opt) {
+				app.fireEvent('errorEvent',
+						'Server-Side error with status code' + response.status);
 			}
 		});
+	},
+
+	loginBtnClicked : function(srcElement) {
+		var win = srcElement.up('window'), form = win.down('form').getForm();
+		win.setLoading();
+		var app = this.application;
+		if (form.isValid()) {
+			Ext.getCmp('loginStatusLabel').hide();
+			form.submit({
+						success : function(form, action) {
+							if (action.result.loggedIn) {
+								app.fireEvent('loggedInEvent',
+										action.result.username);
+								// Login was successful
+								win.hide();
+							} else {
+								// Login failed
+								app.fireEvent('notLoggedInEvent');
+								Ext.getCmp('loginStatusLabel').show();
+							}
+							win.setLoading(false);
+						},
+						failure : function(form, action) {
+							win.setLoading(false);
+							Ext.Msg.alert('Failed', action.result.error);
+						}
+					});
+		}
+	},
+
+	/*
+	 * Functions which will be called on Events.
+	 */
+	onLoggedInEvent : function(username, env) {
+		console.log('LoginController > loggedInEvent');
+		var loggedInTextView = this.getLoggedInText();
+		loggedInTextView.setUsername(username);
+	},
+
+	onNotLoggedInEvent : function() {
+		console.log('LoginController > notLoggedInEvent');
+		var loginView = Ext.widget('loginview');
+		console.log(loginView);
+		loginView.show();
+	}
+});
