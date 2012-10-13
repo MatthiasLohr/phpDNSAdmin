f0a5ca267e2688c32197ea860ada250fbb712bea
Matthias Lohr <matthias@lohr.me>
2012-10-13 15:30:58 +0200

    some new RRTypes

---
+++ api/lib/rrtypes/sshfprecord.class.php
@@ -0,0 +1,43 @@
+<?php
+
+/*
+ * This file is part of phpDNSAdmin.
+ * (c) 2012 Matthias Lohr - http://phpdnsadmin.sourceforge.net/
+ *
+ * phpDNSAdmin is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * phpDNSAdmin is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with phpDNSAdmin. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+/**
+ * @package phpDNSAdmin
+ * @subpackage ResourceRecords
+ * @author Matthias Lohr <matthias@lohr.me>
+ */
+
+/**
+ * @package phpDNSAdmin
+ * @subpackage ResourceRecords
+ * @author Matthias Lohr <matthias@lohr.me>
+ */
+class SshfpRecord extends ResourceRecord {
+
+	public static function listFields() {
+		return array(
+			'algorithm' => 'UInt8',
+			'type' => 'UInt8',
+			'fingerprint' => 'String' /** @todo change to HexString */
+		);
+	}
+}
+
+?>
