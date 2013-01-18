9fa67fd8c6b3ab811bca7658b58ad0eaabe29ec4
Andreas Litt <andreas.litt@gmail.com>
2013-01-18 13:41:34 +0100

    Some changes

--- resources/js/views/zoneitemview.js
+++ resources/js/views/zoneitemview.js
@@ -3,6 +3,11 @@ var ZoneItemView = Backbone.View.extend({
 	tagName: 'li',
 	template: _.template($('#zoneItemViewTemplate').html()),
 
+	events: {
+		'dblclick a': 'changeZoneName',
+		'click a': 'openZone'
+	},
+
 	initialize: function() {
 		this.listenTo(this.model, 'change', this.render);
 	},
@@ -10,5 +15,13 @@ var ZoneItemView = Backbone.View.extend({
 	render: function() {
 		this.$el.html(this.template(this.model.toJSON()));
 		return this;
+	},
+
+	changeZoneName: function() {
+		console.log('dblclicked');
+	},
+
+	openZone: function() {
+		console.log('clicked');
 	}
 });
